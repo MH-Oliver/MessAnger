7515e4c3c06a5e87e66525cabe692cb134f1656e
MH-Oliver <oliver.jockel@gmx.de>
2022-10-22 16:14:48 +0200

    new

--- src/main/java/View/ChatViewController.java
+++ src/main/java/View/ChatViewController.java
@@ -56,11 +56,50 @@ public class ChatViewController {
         System.out.println("Chat: " + MessengerGui.mCurrentContactID);
         lConnector.executeStatement("Select * from Message where (SenderID = " + MessengerGui.mCurrentContactID + " OR SenderID = " + MessengerGui.mCurrentLoggedUserID +  ") AND (EmpfaengerID = " + MessengerGui.mCurrentContactID + " OR EmpfaengerID = " + MessengerGui.mCurrentLoggedUserID + ") ORDER BY Time ASC");
         //System.out.println(lConnector.getErrorMessage());
-        String[][] lMessageResult = lConnector.getCurrentQueryResult().getData();
-        for (int i = 0; i < lMessageResult.length; i++) {
-            //System.out.println(lMessageResult[i][3]);
-            if (Long.parseLong(latestMessageTime) < Long.parseLong(lMessageResult[i][1])) latestMessageTime = lMessageResult[i][1];
-            if (lMessageResult[i][3].equals(MessengerGui.mCurrentContactID)) {
+        addNewMessages(lConnector);
+
+        //Weiter mit sonstigen Buttons
+
+        SendButton.setOnMouseClicked(ev -> {
+            MessengerGui.mEchoClient.send("SendeAnNachricht:" + MessengerGui.mCurrentContactID + ":"+WriteField.getText());
+        });
+
+        Timeline t1 = new Timeline();
+        KeyFrame keyframe =
+                new KeyFrame(Duration.seconds(2), ev -> {
+                    MessengerGui.mEchoClient.send("GetMessages:" + MessengerGui.mCurrentLoggedUserID);
+
+                    lConnector.executeStatement("Select Name from User where ID = '" + MessengerGui.mCurrentContactID + "'");
+
+                    ContactField.setText(lConnector.getCurrentQueryResult().getData()[0][0]);
+                    //System.out.println(latestMessageTime);
+
+                    lConnector.executeStatement("Select * from Message where ((SenderID = " + MessengerGui.mCurrentContactID + " OR SenderID = " + MessengerGui.mCurrentLoggedUserID + ") AND (EmpfaengerID = " + MessengerGui.mCurrentContactID + " OR EmpfaengerID = " + MessengerGui.mCurrentLoggedUserID + ")) AND Time > " +latestMessageTime+ " ORDER BY Time ASC");
+                    addNewMessages(lConnector);
+
+
+
+
+                });
+        t1.getKeyFrames().add(keyframe);
+        t1.setCycleCount(Timeline.INDEFINITE);
+        t1.play();
+
+
+
+
+
+
+                }
+
+    private void addNewMessages(DatabaseConnector lConnector) {
+
+
+        String[][] lNewMessageResult = lConnector.getCurrentQueryResult().getData();
+        for (int i = 0; i < lNewMessageResult.length; i++) {
+            //System.out.println(lNewMessageResult[i][3]);
+            if (Long.parseLong(latestMessageTime) < Long.parseLong(lNewMessageResult[i][1])) latestMessageTime = lNewMessageResult[i][1];
+            if (lNewMessageResult[i][3].equals(MessengerGui.mCurrentContactID)) {
                 // denk an doppeltes Füllen damit immer eine Nachricht eine ganze Zeile füllt (Nachricht - Text)
                 HBox lHBox = new HBox();
                 lHBox.setSpacing(50);
@@ -69,7 +108,7 @@ public class ChatViewController {
                 Rectangle lRectangle = new Rectangle();
 
                 Text lText = new Text();
-                lText.setText(lMessageResult[i][0]);
+                lText.setText(lNewMessageResult[i][0]);
 
                 lRectangle.setArcHeight(10);
                 lRectangle.setArcWidth(10);
@@ -94,7 +133,8 @@ public class ChatViewController {
                 lHBox.getChildren().add(lNameText);
 
                 MessageField.getChildren().add(lHBox);
-            } else {
+            }
+            else {
 
                 HBox lHBox = new HBox();
                 lHBox.setSpacing(50);
@@ -110,7 +150,7 @@ public class ChatViewController {
                 Rectangle lRectangle = new Rectangle();
 
                 Text lText = new Text();
-                lText.setText(lMessageResult[i][0]);
+                lText.setText(lNewMessageResult[i][0]);
 
                 lRectangle.setArcHeight(10);
                 lRectangle.setArcWidth(10);
@@ -130,116 +170,7 @@ public class ChatViewController {
 
                 MessageField.getChildren().add(lHBox);
             }
-
-
         }
-
-        //Weiter mit sonstigen Buttons
-
-        SendButton.setOnMouseClicked(ev -> {
-            MessengerGui.mEchoClient.send("SendeAnNachricht:" + MessengerGui.mCurrentContactID + ":"+WriteField.getText());
-        });
-
-        Timeline t1 = new Timeline();
-        KeyFrame keyframe =
-                new KeyFrame(Duration.seconds(2), ev -> {
-                    MessengerGui.mEchoClient.send("GetMessages:" + MessengerGui.mCurrentLoggedUserID);
-
-                    lConnector.executeStatement("Select Name from User where ID = '" + MessengerGui.mCurrentContactID + "'");
-
-                    ContactField.setText(lConnector.getCurrentQueryResult().getData()[0][0]);
-                    //System.out.println(latestMessageTime);
-
-                    lConnector.executeStatement("Select * from Message where ((SenderID = " + MessengerGui.mCurrentContactID + " OR SenderID = " + MessengerGui.mCurrentLoggedUserID + ") AND (EmpfaengerID = " + MessengerGui.mCurrentContactID + " OR EmpfaengerID = " + MessengerGui.mCurrentLoggedUserID + ")) AND Time > " +latestMessageTime+ " ORDER BY Time ASC");
-                    String[][] lNewMessageResult = lConnector.getCurrentQueryResult().getData();
-                    for (int i = 0; i < lNewMessageResult.length; i++) {
-                        //System.out.println(lNewMessageResult[i][3]);
-                        if (Long.parseLong(latestMessageTime) < Long.parseLong(lNewMessageResult[i][1])) latestMessageTime = lNewMessageResult[i][1];
-                        if (lNewMessageResult[i][3].equals(MessengerGui.mCurrentContactID)) {
-                            // denk an doppeltes Füllen damit immer eine Nachricht eine ganze Zeile füllt (Nachricht - Text)
-                            HBox lHBox = new HBox();
-                            lHBox.setSpacing(50);
-
-                            Pane lPane = new Pane();
-                            Rectangle lRectangle = new Rectangle();
-
-                            Text lText = new Text();
-                            lText.setText(lNewMessageResult[i][0]);
-
-                            lRectangle.setArcHeight(10);
-                            lRectangle.setArcWidth(10);
-                            lRectangle.setHeight(50);
-
-                            lRectangle.setFill(Color.LIGHTGREEN);
-                            if (lText.getText().toCharArray().length * 8 < 212.5)
-                                lRectangle.setWidth(lText.getText().toCharArray().length * 4 + 40);
-                            else lRectangle.setWidth(207.5);
-
-                            lHBox.getChildren().add(lPane);
-                            lPane.getChildren().add(lRectangle);
-                            lPane.getChildren().add(lText);
-                            lText.setX(lRectangle.getX() + lRectangle.getWidth() / 2 - lText.getText().toCharArray().length * 4);
-                            lText.setY(lRectangle.getY() + lRectangle.getHeight() / 2);
-
-                            //Teil 2
-                            Text lNameText = new Text();
-                            lConnector.executeStatement("Select Name from User where ID = '" + MessengerGui.mCurrentContactID + "'");
-                            //lNameText.setText(lConnector.getCurrentQueryResult().getData()[0][0]);
-                            lNameText.setText("                                            ");
-                            lHBox.getChildren().add(lNameText);
-
-                            MessageField.getChildren().add(lHBox);
-                        }
-                        else {
-
-                            HBox lHBox = new HBox();
-                            lHBox.setSpacing(50);
-
-                            Text lNameText = new Text();
-                            lConnector.executeStatement("Select Name from User where ID = '" + MessengerGui.mCurrentLoggedUserID + "'");
-                            //lNameText.setText(lConnector.getCurrentQueryResult().getData()[0][0]);
-                            lNameText.setText("                                            ");
-                            lHBox.getChildren().add(lNameText);
-
-
-                            Pane lPane = new Pane();
-                            Rectangle lRectangle = new Rectangle();
-
-                            Text lText = new Text();
-                            lText.setText(lMessageResult[i][0]);
-
-                            lRectangle.setArcHeight(10);
-                            lRectangle.setArcWidth(10);
-                            lRectangle.setHeight(50);
-
-                            lRectangle.setFill(Color.LIGHTBLUE);
-                            if (lText.getText().toCharArray().length * 8 < 212.5)
-                                lRectangle.setWidth(lText.getText().toCharArray().length * 4 + 40);
-                            else lRectangle.setWidth(207.5);
-
-                            lHBox.getChildren().add(lPane);
-                            lPane.getChildren().add(lRectangle);
-                            lPane.getChildren().add(lText);
-                            lText.setX(lRectangle.getX() + lRectangle.getWidth() / 2 - lText.getText().toCharArray().length * 4);
-                            lText.setY(lRectangle.getY() + lRectangle.getHeight() / 2);
-
-
-                            MessageField.getChildren().add(lHBox);
-                        }
-                    }
-
-
-
-                });
-        t1.getKeyFrames().add(keyframe);
-        t1.setCycleCount(Timeline.INDEFINITE);
-        t1.play();
-
-
-
-
-
-
-                }
+    }
     }
 
